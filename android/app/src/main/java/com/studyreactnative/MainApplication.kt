89cd620f5fcc86679ed1d250c85780afa3967198
rikenmi <rkm0219108@gmail.com>
2020-11-28 17:28:13 +0900

    android kotlin

---
+++ android/app/src/main/java/com/studyreactnative/MainApplication.kt
@@ -0,0 +1,46 @@
+package com.studyreactnative
+
+import android.app.Application
+import android.content.Context
+import com.facebook.react.*
+import com.facebook.soloader.SoLoader
+
+class MainApplication : Application(), ReactApplication {
+    private val mReactNativeHost: ReactNativeHost = object : ReactNativeHost(this) {
+        override fun getUseDeveloperSupport(): Boolean {
+            return BuildConfig.DEBUG
+        }
+
+        override fun getPackages(): List<ReactPackage> {
+            // Packages that cannot be autolinked yet can be added manually here, for example:
+            // packages.add(new MyReactNativePackage());
+            return PackageList(this).packages
+        }
+
+        override fun getJSMainModuleName(): String {
+            return "index"
+        }
+    }
+
+    override fun getReactNativeHost(): ReactNativeHost {
+        return mReactNativeHost
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this,  /* native exopackage */false)
+        initializeFlipper(this, reactNativeHost.reactInstanceManager)
+    }
+
+    companion object {
+        /**
+         * Loads Flipper in React Native templates. Call this in the onCreate method with something like
+         * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+         */
+        private fun initializeFlipper(context: Context, reactInstanceManager: ReactInstanceManager) {
+            if (BuildConfig.DEBUG) {
+                ReactNativeFlipper.initializeFlipper(context, reactInstanceManager)
+            }
+        }
+    }
+}
