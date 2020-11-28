836dc68f4bc330303e916c21b28efca75222f44b
rikenmi <rkm0219108@gmail.com>
2020-11-28 14:01:30 +0900

    demo

---
+++ src/Examples/Dialogs/index.tsx
@@ -0,0 +1,5 @@
+export { default as DialogWithCustomColors } from './DialogWithCustomColors';
+export { default as DialogWithLoadingIndicator } from './DialogWithLoadingIndicator';
+export { default as DialogWithLongText } from './DialogWithLongText';
+export { default as DialogWithRadioButtons } from './DialogWithRadioButtons';
+export { default as UnDismissableDialog } from './UnDismissableDialog';
